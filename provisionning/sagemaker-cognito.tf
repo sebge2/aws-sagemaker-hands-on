74917de12191bea5c499025c9c5d6ba97019bfef
Sébastien Gérard <me@sgerard.be>
2023-05-24 21:20:05 +0200

    cognito

--- provisionning/sagemaker-cognito.tf
+++ provisionning/sagemaker-cognito.tf
@@ -16,6 +16,35 @@ resource "aws_cognito_user" "john_doe" {
   }
 }
 
+resource "aws_cognito_user" "jane_doe" {
+  user_pool_id = aws_cognito_user_pool.test.id
+  username     = "jane.doe"
+  password     = "T€st-123456"
 
+  attributes = {
+    email          = "jane.doe@acme.com"
+    email_verified = true
+  }
+}
 
+resource "aws_cognito_user_pool_client" "test" {
+  name         = var.resources_name
+  user_pool_id = aws_cognito_user_pool.test.id
+}
 
+resource "aws_cognito_user_group" "test" {
+  user_pool_id = aws_cognito_user_pool.test.id
+  name         = var.resources_name
+}
+
+resource "aws_cognito_user_in_group" "john_doe_membership" {
+  user_pool_id = aws_cognito_user_pool.test.id
+  group_name   = aws_cognito_user_group.test.name
+  username     = aws_cognito_user.john_doe.username
+}
+
+resource "aws_cognito_user_in_group" "jane_doe_membership" {
+  user_pool_id = aws_cognito_user_pool.test.id
+  group_name   = aws_cognito_user_group.test.name
+  username     = aws_cognito_user.jane_doe.username
+}
